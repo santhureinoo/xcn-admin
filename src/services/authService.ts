2b8536d65f394c3de0aa94c12170addd22fd8f7a
San_ThureinOo <santhueinoo@gmail.com>
2025-06-10 18:26:07 +0700

    chore: update backend api

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,4 +1,4 @@
-const BASE_API_URL = 'http://localhost:3001';
+const BASE_API_URL = 'https://916e-2a09-bac1-6f20-00-2e5-72.ngrok-free.app';
 
 // Define types for the auth responses
 export interface User {
